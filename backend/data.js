b486e7baf4947c8d9ae20d8d66fc662a45823238
WanderWashington <wander.wsantos@gmail.com>
2020-09-06 21:32:49 -0300

    v0.70

---
+++ backend/data.js
@@ -0,0 +1,23 @@
+export default{
+    products:[{
+        _id:'1',
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:39,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+    }, 
+    {
+        _id:'2',
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+    }
+]
+}
